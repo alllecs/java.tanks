cca569256b26ad23908c88265234cce4028ca71f
Alexander Smirnov <alllecs@yandex.ru>
2017-06-22 11:41:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Create game "Tanks" on guide TheByteGuru on link:https://www.youtube.com/channel/UCQ_FVsybLSxo-lN9GGjsaD
---
+++ Tanks.java
@@ -0,0 +1,5 @@
+public class Tanks {
+	public static void main(String[] args) {
+		Display.create(600, 600, "Tanks");
+	}
+}
