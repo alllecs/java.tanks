6161e868ba193773b0d7a9f819d7470a39d1b8d5
Alexander Smirnov <alllecs@yandex.ru>
2017-06-22 15:14:10 +0300

    Fix graphics and add animation

--- Display.java
+++ Display.java
@@ -1,8 +1,12 @@
 import javax.swing.JFrame;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.Arrays;
+
 
 public abstract class Display extends Tanks{
 
@@ -10,7 +14,14 @@ public abstract class Display extends Tanks{
 	private static JFrame window;
 	private static Canvas content;
 
-	public static void create(int width, int height, String title) {
+	// создаем буффер
+	private static BufferedImage buffer;
+	private static int[] bufferData;
+	private static Graphics bufferGraphics;
+	private static int clearColor;
+	private static float delta = 0;
+
+	public static void create(int width, int height, String title, int _clearColor) {
 
 		if (created)
 			return;
@@ -19,16 +30,10 @@ public abstract class Display extends Tanks{
 		// exit whith close window
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		content = new Canvas() {
-			public void paint(Graphics g) {
-				super.paint(g);
-				render(g);
-			}				
-		};
+		content = new Canvas();
 		// size window
 		Dimension size = new Dimension(width, height);
 		content.setPreferredSize(size);
-		content.setBackground(Color.black);
 
 		// пользователь не может менять размер окна
 		window.setResizable(false);
@@ -40,15 +45,35 @@ public abstract class Display extends Tanks{
 		window.setLocationRelativeTo(null);
 		// окно видимо
 		window.setVisible(true);
+
+		buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		// достать цвет 
+		bufferData = ( (DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+		// достать объект с изображения
+		bufferGraphics = buffer.getGraphics();
+		clearColor = _clearColor;
+
+		created = true;
+
+	}
+
+	public static void clear() {
+		// заполнение массива значениями вместо "for loop"
+		Arrays.fill(bufferData, clearColor);
 	}
 
 	public static void render() {
-		content.repaint();
+		bufferGraphics.setColor(new Color(0xff0000ff));
+		bufferGraphics.fillOval((int)(250 + (Math.sin(delta) * 200)), 250, 100, 100);
+		delta += 0.02f;
+		
 	}
 
-	private static void render(Graphics g) {
-		g.setColor(Color.white);
-		g.fillOval(300 - 50, 300 - 50, 100, 100);
+	public static void swapBuffers() {
+		// достали графику
+		Graphics g = content.getGraphics();
+		// что хотим нарисовать, координаты, ширина и высота
+		g.drawImage(buffer, 0, 0, null);
 	}
 
 }
--- Tanks.java
+++ Tanks.java
@@ -5,13 +5,15 @@ import java.awt.event.ActionEvent;
 
 public class Tanks {
 	public static void main(String[] args) {
-		Display.create(600, 600, "Tanks");
+		Display.create(600, 600, "Tanks", 0xff00ff00);
 
 		// Таймер 60 раз в секунде (примерно). 
 		Timer t = new Timer(1000 / 60, new AbstractAction() {
 			// функция вызывается каждый интервал времени
 			public void actionPerformed(ActionEvent e) {
+				Display.clear();
 				Display.render();
+				Display.swapBuffers();
 			}
 
 		});
